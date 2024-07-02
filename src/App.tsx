b06047570b49351323a38a3e45da29333db6a8d4
Bektas Keldibayev <bektaskeld@gmail.com>
2024-07-02 10:25:00 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { TimestampText } from "./TimestampText";
+
+import { Gemini } from "./Gemini";
+
+function App() {
+  return (
+    <div>
+      <Gemini />
+      <TimestampText />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/atoms.tsx
@@ -0,0 +1,7 @@
+import { FileMetadataResponse } from "@google/generative-ai/files";
+import { atom } from "jotai";
+
+export const videoFileAtom = atom<File | null>(null);
+export const uploadResultAtom = atom<FileMetadataResponse | null>(null);
+export const promptAtom = atom<string>("");
+export const timestampTextAtom = atom("");
