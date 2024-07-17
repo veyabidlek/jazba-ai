ef766e15137d2c3f54f8f2a0914766c5053f963d
Bektas Keldibayev <bektaskeld@gmail.com>
2024-07-17 13:01:57 +0500

    add recent notes display

--- client/src/app/components/notesContainer.tsx
+++ client/src/app/components/notesContainer.tsx
@@ -1,4 +1,7 @@
+"use client";
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 interface NoteProps {
   title: string;
@@ -6,43 +9,49 @@ interface NoteProps {
 }
 
 const Note: React.FC<NoteProps> = ({ title, content }) => (
-  <div className="bg-[#F6DAD7] rounded-lg overflow-hidden mb-4 flex items-center w-full">
-    <div className="p-4">
-      <h3 className="font-bold text-sm lg:text-md tracking-wider">{title}</h3>
-      <p className="text-xs lg:text-sm tracking-wider">{content}</p>
+  <div className="bg-[#F6DAD7] rounded-lg overflow-hidden mb-4 w-full h-[120px]">
+    {" "}
+    {/* Fixed height */}
+    <div className="p-4 h-full">
+      <h3 className="font-bold text-sm lg:text-md tracking-wider ">{title}</h3>
+      <p className="text-xs lg:text-sm tracking-wider ">{content}</p>
     </div>
-    <img
-      className="w-20 h-20 lg:w-28 lg:h-28 bg-gray-600 rounded-lg object-cover"
-      src="https://images.unsplash.com/photo-1455390582262-044cdead277a?fm=jpg&w=3000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d3JpdGluZ3xlbnwwfHwwfHx8MA%3D%3D"
-      alt=""
-    />
   </div>
 );
 
-export default function NotesContainer() {
-  const notes = [
-    {
-      title: "Log in to save your notes",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius eget lorem et efficitur.",
-    },
-    {
-      title: "Log in to save your notes",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius eget lorem et efficitur.",
-    },
+interface Note {
+  id: number;
+  title: string;
+  content: string;
+  date: Date;
+}
 
-    {
-      title: "Log in to save your notes",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius eget lorem et efficitur.",
-    },
-    {
-      title: "Log in to save your notes",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius eget lorem et efficitur.",
-    },
-  ];
+const urleke = process.env.BACKEND_URL;
+
+export default function NotesContainer() {
+  const getNotes = async (): Promise<Note[]> => {
+    try {
+      const response = await axios.get(`${urleke}/api/notes`);
+      const notes = response.data.map((note: any) => ({
+        ...note,
+        date: new Date(note.date),
+      }));
+      return notes;
+    } catch (err) {
+      console.error("Notes ala almai kadym bez obid");
+      return [];
+    }
+  };
+
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const data = await getNotes();
+      setNotes(data.slice(0, 4)); // Limit to 4 notes
+    };
+    fetchNotes();
+  }, []);
 
   return (
     <div className="bg-[#D34836] p-6 rounded-2xl flex flex-col h-[700px] w-full lg:w-[500px]">
