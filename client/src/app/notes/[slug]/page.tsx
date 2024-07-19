0e083818e3ce51c3979eb809438f2ade627f8ce7
Bektas Keldibayev <bektaskeld@gmail.com>
2024-07-19 10:42:48 +0500

    fixes sing up button

--- client/src/app/notes/[slug]/page.tsx
+++ client/src/app/notes/[slug]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,80 +1,85 @@
 "use client";
-import React, { useState } from "react";
 import Link from "next/link";
+import { Label } from "../../components/ui/label";
+import { Input } from "../../components/ui/input";
+import { Button } from "../../components/ui/button";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import axios from "axios";
-import { jwtDecode } from "jwt-decode";
 import "dotenv/config";
-
+import axios from "axios";
 const urleke = process.env.BACKEND_URL;
 
-export default function Login() {
+export default function Register() {
   const router = useRouter();
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+  const [password2, setPassword2] = useState("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!isPasswordSame(password, password2)) {
+      alert("Passwords are not same");
+      return;
+    }
     try {
-      const response = await axios.post(`${urleke}/api/login`, {
+      await axios.post(`${urleke}/api/register`, {
         email,
+        username,
         password,
       });
-      localStorage.setItem("token", response.data.accessToken);
-      const token = localStorage.getItem("token");
-      if (!token) {
-        throw new Error("not logged in");
-      }
-      const user = jwtDecode(token);
-      if (!response) {
-        alert("Wrong email or password");
-      }
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      alert(`Welcome ${user.username}`);
-      router.push("/");
+      alert("Successfull registration");
+      router.push("/login");
     } catch (err) {
-      console.error("Error logging in the user: ", err);
+      console.error("Error sumbitting registration: ", err);
     }
   };
 
-  const togglePasswordVisibility = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword);
+  const isPasswordSame = (password1: string, password2: string): boolean => {
+    if (password1 === password2) {
+      return true;
+    }
+    return false;
   };
 
   return (
     <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-[#244855] px-4 py-12 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-md rounded-lg bg-white p-16 shadow-lg">
+      <div className="mx-auto max-w-md rounded-lg bg-white p-6 shadow-lg">
         <div className="space-y-4 text-center">
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center ">
             <Link
               href="/"
               className="p-2 rounded-full text-white hover:bg-muted p-2 rounded-md bg-[#D34836]"
               prefetch={false}
             >
-              <ArrowLeftIcon className="h-5 w-5" />
+              <ArrowLeftIcon className="h-5 w-5 " />
               <span className="sr-only">Go back</span>
             </Link>
-            <h1 className="text-3xl font-bold text-[#D34836] ml-[-36px]">
-              Login
+            <h1 className="text-3xl font-bold text-[#D34836] ml-[-32px]">
+              Sign Up
             </h1>
             <div />
           </div>
           <p className="text-muted-foreground">
-            Enter your email and password to login.
-          </p>
-          <p className="inline">
-            Don't have an account?{" "}
-            <Link prefetch={false} href="/register" className="underline">
-              Sign up
+            Already have an account?
+            <Link href="/login" className="ml-2 underline" prefetch={false}>
+              Sign in
             </Link>
           </p>
         </div>
         <form onSubmit={handleSubmit} className="mt-6 space-y-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <Label htmlFor="name">Username</Label>
+              <Input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                id="usernname"
+                placeholder="Enter your username"
+                required
+              />
+            </div>
+          </div>
           <div>
             <Label htmlFor="email">Email</Label>
             <Input
@@ -91,28 +96,29 @@ export default function Login() {
               <Label htmlFor="password">Password</Label>
             </div>
             <Input
-              type={showPassword ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               id="password"
               placeholder="Enter your password"
               required
             />
-            <Button
-              type="button"
-              onClick={togglePasswordVisibility}
-              variant="ghost"
-              size="icon"
-              className="absolute bottom-1 right-1 h-7 w-7"
-            >
-              <span className="sr-only">Toggle password visibility</span>
-            </Button>
+          </div>
+          <div>
+            <Label htmlFor="confirm-password">Confirm Password</Label>
+            <Input
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)}
+              id="confirm-password"
+              type="password"
+              placeholder="Confirm your password"
+              required
+            />
           </div>
           <Button
             type="submit"
             className="w-full bg-[#D34836] text-white hover:bg-[#c03730] focus:ring-[#D34836]"
           >
-            Login
+            Sign Up
           </Button>
           <div className="relative">
             <div className="absolute inset-0 flex items-center">
