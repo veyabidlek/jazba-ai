0f0a33418a25877feaf2544c27b541135ebbdfc6
Bektas Keldibayev <bektaskeld@gmail.com>
2024-07-23 20:10:23 +0500

    trynna fix deploy

--- client/src/app/components/FloatingButtons.tsx
+++ client/src/app/components/FloatingButtons.tsx
@@ -1,12 +1,10 @@
 import { CogIcon, QuestionMarkCircleIcon } from "@heroicons/react/outline";
 
-const FloatingButtons = () => {
+export default function FloatingButtons() {
   return (
     <div className="fixed bottom-4 right-4 flex flex-col space-y-2 ">
       <QuestionMarkCircleIcon className="h-10 w-10 text-white hover:scale-105 hover:shadow-x cursor-pointer transition-all duration-300 hover:text-black " />
       <CogIcon className="h-10 w-10 text-white hover:scale-105 hover:shadow-x cursor-pointer transition-all duration-300 hover:text-black" />
     </div>
   );
-};
-
-export default FloatingButtons;
+}
