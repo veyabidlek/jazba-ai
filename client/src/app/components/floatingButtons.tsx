6e2398407cef76343cdba695e4e6f864b795a6a6
veyabidlek <bektas.keldibyaev@nu.edu.kz>
2024-08-22 10:08:28 +0500

    fixes floating buttons click on mobile

--- client/src/app/components/floatingButtons.tsx
+++ client/src/app/components/floatingButtons.tsx
@@ -2,7 +2,7 @@ import { QuestionButton } from "./questionButton";
 import { SettingsButton } from "./settingsButton";
 export function FloatingButtons() {
   return (
-    <div className="fixed bottom-4 right-4 flex flex-col space-y-2 ">
+    <div className="fixed bottom-4 right-4 flex flex-col space-y-2 z-50">
       <QuestionButton />
       <SettingsButton />
     </div>
